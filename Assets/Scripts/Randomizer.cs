7987c5231e9b4a399ca5bc196f6c241cf871cc0f
WengerM996 <maks.wenger@yandex.ru>
2021-02-05 09:11:24 +0300

    init

---
+++ Assets/Scripts/Randomizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Randomizer : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private List<Transform> _points;
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        Instantiate(_template, GetRandomPoint(), Quaternion.identity);
+    }
+
+    private Vector3 GetRandomPoint() { return _points[Random.Range(0, _points.Count)].position; }
+}
